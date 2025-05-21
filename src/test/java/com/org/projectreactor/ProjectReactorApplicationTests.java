39fc18339e779f2807756a1fe528af9ef4ea754e
Jayesh <jayesh.swami@cern.ch>
2025-05-21 14:07:26 +0200

    first

---
+++ src/test/java/com/org/projectreactor/ProjectReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.projectreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
